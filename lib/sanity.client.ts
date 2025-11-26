825354671789f77083db65e0a8109c25c14fcbcd
lazola sonqishe <lazolasonqishe@lazolas-MacBook-Air.local>
2025-11-26 15:32:16 +0200

    Remove unused Sanity CMS

--- lib/sanity.client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client'
-
-export const sanityClient = createClient({
-  projectId: 'y5scr6e4',
-  dataset: 'production',
-  apiVersion: '2025-11-18',
-  useCdn: true,
-})
